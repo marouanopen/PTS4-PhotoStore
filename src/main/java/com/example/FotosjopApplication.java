d0628985018512915f04175dc703f89d668c2ae3
Marouan Bakour <marouano@FreeThinker.local>
2017-03-20 12:10:53 +0100

    Initial project

---
+++ src/main/java/com/example/FotosjopApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotosjopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotosjopApplication.class, args);
+	}
+}
