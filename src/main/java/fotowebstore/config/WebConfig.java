00b4006bc1c3e4de0da0b549eb4c4b734ddbcf8d
Peter van Bijsterveldt <petervbijsterveldt@gmail.com>
2017-03-26 17:07:15 +0200

    File upload + pixelation

--- src/main/java/fotowebstore/config/WebConfig.java
+++ src/main/java/fotowebstore/config/WebConfig.java
@@ -3,6 +3,8 @@ package fotowebstore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -13,10 +15,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class WebConfig extends WebMvcConfigurerAdapter{
 
     @Bean
-    public InternalResourceViewResolver resolver() {
+    public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 }
--- src/main/java/fotowebstore/config/WebInit.java
+++ src/main/java/fotowebstore/config/WebInit.java
@@ -3,9 +3,14 @@ package fotowebstore.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
 @Configuration
 public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    private int maxUploadSizeInMb = 5 * 1024 * 1024; // 5 MB
 
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{RootConfig.class};
@@ -17,5 +22,15 @@ public class WebInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
+        MultipartConfigElement multipartConfigElement =
+                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
+                        maxUploadSizeInMb, maxUploadSizeInMb * 2, maxUploadSizeInMb / 2);
+
+        registration.setMultipartConfig(multipartConfigElement);
     }
 }
